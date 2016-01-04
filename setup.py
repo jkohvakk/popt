e8d97621383482a179b0256c65fe7ec85d06c9b8
Janne Kohvakka <jkohvakk@gmail.com>
2016-01-04 13:32:35 +0200

    Bumped revision

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(name='popt',
       packages=['popt'],
-      version='0.1',
+      version='0.2',
       description='Tool for converting Robot Framework xml output to a human-readable log file',
       author='Janne Kohvakka',
       author_email='jkohvakk@gmail.com',
       url='https://github.com/jkohvakk/popt',
-      download_url='https://github.com/jkohvakk/mypackage/tarball/0.1',
+      download_url='https://github.com/jkohvakk/mypackage/tarball/0.2',
       keywords=['Robot Framework', 'log'],
       classifiers=[],
       entry_points={'console_scripts': ['popt=popt:read_arguments']})
